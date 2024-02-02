9db81f1533a3facbbea2acb7baf3f6b1d0005d12
sameerg1810 <64128604+sameerg1810@users.noreply.github.com>
2024-02-02 13:28:48 +0530

    configured the userController

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,5 +1,5 @@
 // authController.js
-import User from "../models/userModel.js";
+import User from "../models/UserModel.js";
 import argon2 from "argon2";
 
 export const signup = async (req, res) => {
